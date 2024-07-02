bc569f6fe816b40911659d37d11a761fdf3e9e53
glenndsilva1 <glenndsilva@rediffmail.com>
2024-07-02 12:05:56 +0530

    PgArticle.java

---
+++ PgArticle.java
@@ -0,0 +1,8 @@
+package com.servlet;
+
+public class PgArticle implements Article{
+	// Here the entire article is given along with author and name
+	public String head() {
+		return " head";
+	}
+}
